1714954a9975af247ff7a36f4f872032e03656b9
MarioDena <enrique.dn@outlook.com>
2020-09-27 13:39:56 -0500

    [Feature] Finishes projects section

--- src/reducers/ProjectsReducer.js
+++ src/reducers/ProjectsReducer.js
@@ -8,7 +8,8 @@ import list from '../assets/list.png';
 const projects = [
   {
     title: 'BLOG',
-    description: 'A personal blog built with Jekyll as a static site builder and markdown for content creation.',
+    description:
+      'A personal blog built with Jekyll as a static site builder and markdown for content creation.',
     imageURL: blog,
     projectLink: 'https://mariodena.github.io/blog/',
     github: 'https://github.com/MarioDena/blog',
@@ -18,16 +19,16 @@ const projects = [
     title: 'Bookstore CMS',
     description: `This project is a pure Javascript game DEMO (Phaser), using API's to store and retrieve scores.`,
     imageURL: book,
-    projectLink: '',
-    github: '',
+    projectLink: 'https://cms-mariodena.herokuapp.com/',
+    github: 'https://github.com/MarioDena/bookstore-cms',
     tech: ['Javascript', 'React', 'Redux', 'SASS', 'Bootstrap'],
   },
   {
     title: 'RPG DEMO',
     description: `This project is a pure Javascript game DEMO (Phaser), using API's to store and retrieve scores.`,
     imageURL: rpg,
-    projectLink: '',
-    github: '',
+    projectLink: 'https://mariodena.github.io/RPGdemo/',
+    github: 'https://github.com/MarioDena/JSCapstone',
     tech: ['E6S', 'Javascript', 'Phaser', 'Webpack'],
   },
   {
@@ -35,23 +36,24 @@ const projects = [
     description: `A colaborative project for a TODO list, build with pure Javascript/E6S works using the local storage on the web-browser.`,
     imageURL: list,
     projectLink: '',
-    github: '',
+    github: 'https://github.com/Luzaks/TodoList',
     tech: ['Javascript', 'Bootstrap', 'Local Storage'],
   },
   {
     title: 'RECIPI',
     description: `Recipi is a simple project made to demostrate the usage of API's on a React-Redux enviroment.`,
     imageURL: recipi,
-    projectLink: '',
-    github: '',
+    projectLink: 'https://infinite-atoll-25264.herokuapp.com/',
+    github: 'https://github.com/MarioDena/recipi/tree/development',
     tech: ['Javascript', 'React', 'Redux', 'Mobile', 'Axios', 'Bootstrap'],
   },
   {
     title: 'Twitter Bot',
-    description: 'A bot that opens up a browser, sing in to Twitter, search for a topic and likes tweets. (Be aware that this bot can get your account banned)',
+    description:
+      'A bot that opens up a browser, sing in to Twitter, search for a topic and likes tweets. (Be aware that this bot can get your account banned)',
     imageURL: bot,
     projectLink: '',
-    github: '',
+    github: 'https://github.com/MarioDena/TwitterBot',
     tech: ['Phyton', 'Selenium'],
   },
 ];
