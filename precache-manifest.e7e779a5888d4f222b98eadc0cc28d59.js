782b9dfef512835d32bad7bcfa9252616c1c78e7
MarioDena <enrique.dn@outlook.com>
2020-09-29 00:52:10 -0500

    Updates

---
+++ precache-manifest.e7e779a5888d4f222b98eadc0cc28d59.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae8db21359b85cc0c4e14ac84664a317",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "88c05200b6e9fe29c558",
+    "url": "/portfolio/static/css/main.ed5112c2.chunk.css"
+  },
+  {
+    "revision": "f0cdadffca0c21ede009",
+    "url": "/portfolio/static/js/2.bc9bae28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.bc9bae28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88c05200b6e9fe29c558",
+    "url": "/portfolio/static/js/main.5b90b35d.chunk.js"
+  },
+  {
+    "revision": "37198e31f2a2da930abf",
+    "url": "/portfolio/static/js/runtime-main.a9e7c63b.js"
+  },
+  {
+    "revision": "8376d4effad2f92ea42f3e2c93b302c9",
+    "url": "/portfolio/static/media/Foto.8376d4ef.jpg"
+  },
+  {
+    "revision": "aabd1154e64850ef142880c786a79d82",
+    "url": "/portfolio/static/media/Resume.aabd1154.pdf"
+  },
+  {
+    "revision": "abbf2e4904aaacee66f7934d2c45e00d",
+    "url": "/portfolio/static/media/blog.abbf2e49.png"
+  },
+  {
+    "revision": "a30c7f6f58e303a5dcc7fa9050a31593",
+    "url": "/portfolio/static/media/bookstore.a30c7f6f.png"
+  },
+  {
+    "revision": "16b4b95b6b80fed1da17e87217ddf771",
+    "url": "/portfolio/static/media/bot.16b4b95b.png"
+  },
+  {
+    "revision": "55b6ea8d5290df52bca4ab8a3402e9da",
+    "url": "/portfolio/static/media/list.55b6ea8d.png"
+  },
+  {
+    "revision": "74e0596a0aee5167e8d85bcade5d93e9",
+    "url": "/portfolio/static/media/main.74e0596a.png"
+  },
+  {
+    "revision": "0378eaf7c48c43e5a3ae717f1588cd28",
+    "url": "/portfolio/static/media/recipi.0378eaf7.png"
+  },
+  {
+    "revision": "09d6faee87ef3df8f16652c2b0764d45",
+    "url": "/portfolio/static/media/rpg.09d6faee.png"
+  }
+]);
